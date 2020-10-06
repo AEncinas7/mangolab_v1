5ed5dccd470d809da832b4d65a9208f09d1de3a9
Alexander Encinas <alexcromwell777@gmail.com>
2020-10-06 05:19:21 -0400

    views and controller setup completed

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,10 @@
 Rails.application.routes.draw do
-  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+
+root to: 'home#index'
+
+resources :home, only: [:index]
+resources :services, only: [:index]
+resources :process, only: [:index]
+resources :verticals, only: [:index]
+
 end
