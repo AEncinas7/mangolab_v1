70990577a32aa97064026ac6d66b63e7ba66243d
Alexander Encinas <alexcromwell777@gmail.com>
2020-10-13 17:53:18 -0400

    head updated

--- app/javascript/components/google_analytics.js
+++
@@ -1,8 +0,0 @@
-const initGoogleAnalytics = () => {
-  window.dataLayer = window.dataLayer || [];
-  function gtag() { dataLayer.push(arguments); }
-  gtag('js', new Date());
-  gtag('config', 'UA-179873545-1');
-}
-
-export { initGoogleAnalytics };
