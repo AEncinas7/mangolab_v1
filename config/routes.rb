7c0eb158c790270ab958729da06321516d88489b
Alexander Encinas <alexcromwell777@gmail.com>
2020-11-12 17:28:30 -0500

    progress

--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,14 @@
 Rails.application.routes.draw do
 
-root to: 'home#index'
-
-resources :home, only: [:index]
-resources :services, only: [:index]
-resources :process, only: [:index]
-resources :industries, only: [:index]
-resources :webinars, only: [:index]
+  # get 'errors/not_found'
+  match '/404', to: 'errors#not_found', via: :all
+  root to: 'home#index'
+  
+  resources :home, only: [:index]
+  resources :services, only: [:index]
+  resources :process, only: [:index]
+  resources :industries, only: [:index]
+  resources :webinars, only: [:index]
+  
 
 end
